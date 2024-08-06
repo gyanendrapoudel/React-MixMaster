156d3043de75f327aa8e8c368abb10b53f85b835
Gyanendra <gyanen56@gmail.com>
2024-08-06 21:01:29 +1000

    using pagination on drinks

--- src/componets/CocktailList.jsx
+++ src/componets/CocktailList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import SingleCockTail from "./SingleCockTail"
 
 const CocktailList = ({drinks}) => {
@@ -5,13 +6,40 @@ const CocktailList = ({drinks}) => {
      const {idDrink:id, strDrink:name, strDrinkThumb:image, strAlcoholic:info, strGlass:glass } = item
      return {id,name, image,info, glass}
  })
- 
+const drinkPerPage = 8
+const numberOfPage = Math.ceil(formattedDrinks.length/10)
+const newDrinksList = Array.from({length:numberOfPage},(_,index)=>{
+  const start = index*drinkPerPage;
+  return formattedDrinks.slice(start,start+drinkPerPage)
+})
+const [page, setPage] = useState(0)
+console.log(newDrinksList)
   return (
-    <div className="cocktails">
-      {formattedDrinks.map((item) => {
-        return <SingleCockTail key={item.id} {...item} />
-      })}
-    </div>
+    <>
+      <div className="cocktails">
+        
+          {newDrinksList[page].map((item) => {
+            return <SingleCockTail key={item.id} {...item} />
+          })}
+          </div>
+          <div className="d-flex justify-content-center gap-2 mt-5">
+          
+            {newDrinksList.map((item, i) => {
+              return (
+                <button className="btn btn-primary" key={i} onClick={() => setPage(i)}>
+                  {i + 1}
+                </button>
+              )
+            })}
+          </div>
+      
+    </>
+
+    // <div className="cocktails">
+    //   {formattedDrinks.map((item) => {
+    //     return <SingleCockTail key={item.id} {...item} />
+    //   })}
+    // </div>
   )
 }
 export default CocktailList
